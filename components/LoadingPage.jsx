a14a63ff440eb8f921fac2d6459ba9d037020eae
Jose Soto <jm.soto.sanchez@gmail.com>
2021-08-14 18:25:10 -0300

    adjust margin top

--- components/LoadingPage.jsx
+++ components/LoadingPage.jsx
@@ -2,7 +2,7 @@ import { Center, Spinner } from '@chakra-ui/react';
 
 const LoadingPage = () => {
   return (
-    <Center w="100%" h="100vh">
+    <Center w="100%" mt="20%">
       <Spinner size="xl"></Spinner>
     </Center>
   );
