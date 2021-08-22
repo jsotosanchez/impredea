30e99c78b087e2afb16e1936237cc997960da7cf
Jose Soto <jm.soto.sanchez@gmail.com>
2021-08-22 18:08:10 -0300

    generic error component

---
+++ components/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import { Center, Heading, Spinner } from '@chakra-ui/react';
+
+const ErrorPage = () => {
+  return (
+    <Center w="100%" mt="20%">
+      <Heading as="h1" size="md">
+        Lo sentimos, ha ocurrido un error.
+      </Heading>
+      <Text>Por favor intenta mas tarde</Text>
+    </Center>
+  );
+};
+
+export default ErrorPage;
