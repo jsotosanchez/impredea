5f22161fac218736791a473aeeb19ada2c19da05
Jose Soto <jm.soto.sanchez@gmail.com>
2021-08-22 18:29:11 -0300

    create index for components

---
+++ components/index.js
@@ -0,0 +1,37 @@
+import Authorization from './Authorization';
+import CatalogAdmin from './CatalogAdmin';
+import ClientOnly from './ClientOnly';
+import ErrorPage from './ErrorPage';
+import Layout from './Layout';
+import LoadingPage from './LoadingPage';
+import MakeQuestionModal from './MakeQuestionModal';
+import MakerCard from './MakerCard';
+import MakerInfoAdmin from './MakerInfoAdmin';
+import MakerSalesAdmin from './MakerSalesAdmin';
+import ManageProductModal from './ManageProductModal';
+import ProductCard from './ProductCard';
+import QuestionCard from './QuestionCard';
+import QuestionsAdmin from './QuestionsAdmin';
+import QuotationAdmin from './QuotationAdmin';
+import RenderRating from './RenderRating';
+import ReviewCard from './ReviewCard';
+
+export {
+  Authorization,
+  CatalogAdmin,
+  ClientOnly,
+  ErrorPage,
+  Layout,
+  LoadingPage,
+  MakeQuestionModal,
+  MakerCard,
+  MakerInfoAdmin,
+  MakerSalesAdmin,
+  ManageProductModal,
+  ProductCard,
+  QuestionCard,
+  QuestionsAdmin,
+  QuotationAdmin,
+  RenderRating,
+  ReviewCard,
+};
