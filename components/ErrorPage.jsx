164db41c801aa8c06bf7473454ffc73ceefd3c30
Jose Soto <jm.soto.sanchez@gmail.com>
2021-09-22 10:40:12 -0300

    Error page

--- components/ErrorPage.jsx
+++ components/ErrorPage.jsx
@@ -1,12 +1,19 @@
-import { Center, Heading } from '@chakra-ui/react';
+import { Center, Heading, Text, Stack, Button } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 
 const ErrorPage = () => {
+  const router = useRouter();
   return (
     <Center w="100%" mt="20%">
-      <Heading as="h1" size="md">
-        Lo sentimos, ha ocurrido un error.
-      </Heading>
-      <Text>Por favor intenta mas tarde</Text>
+      <Stack>
+        <Heading as="h1" size="md">
+          Lo sentimos, ha ocurrido un error.
+        </Heading>
+        <Text>Por favor intenta mas tarde</Text>
+        <Button variant="link" colorScheme="facebook" onClick={() => router.push('/')}>
+          Volver a inicio
+        </Button>
+      </Stack>
     </Center>
   );
 };
