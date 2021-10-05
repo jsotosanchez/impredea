a99a8e31af764fec8fb92993cf5f9be3095db5fc
Jose Soto <jm.soto.sanchez@gmail.com>
2021-10-05 01:16:36 -0300

    change to ts

--- components/common/SideBarLayout.tsx
+++ components/common/SideBarLayout.tsx
@@ -1,11 +1,9 @@
 import { Flex, Stack } from '@chakra-ui/react';
-/**
- *
- * @param {JSX.Element} sideBarChildren
- * @param {JSX.Element} contentChildren
- * @returns {JSX.Element}
- */
-const SideBarLayout = ({ sideBarChildren, contentChildren }) => (
+interface Props {
+  sideBarChildren: JSX.Element;
+  contentChildren: JSX.Element;
+}
+const SideBarLayout = ({ sideBarChildren, contentChildren }: Props): JSX.Element => (
   <Flex w="100%" h="100vh" pt="4rem">
     <Stack w="25%" bg="brandGray.100" v="100vh" p="2rem 3rem">
       {sideBarChildren}
