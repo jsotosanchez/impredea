c307fb6669ea4d54238292e0279ec356c687f978
Jose Soto <jm.soto.sanchez@gmail.com>
2021-10-05 01:26:24 -0300

    change to ts

--- pages/maker/[id]/catalog.tsx
+++ pages/maker/[id]/catalog.tsx
@@ -7,8 +7,9 @@ import { GET_MAKER_CATALOG } from '@/graphql/queries';
 import { Layout } from '@/components/makerPage';
 import { LoadingPage } from '@/components/common';
 import { MAKER_SECTIONS } from '@/utils/constants';
+import { Product } from 'types';
 
-export default function Catalog() {
+export default function Catalog(): JSX.Element {
   const router = useRouter();
   const { id } = router.query;
   const { data, loading, refetch } = useQuery(GET_MAKER_CATALOG, { variables: { id } });
@@ -27,11 +28,11 @@ export default function Catalog() {
   return (
     <Layout activeHeader={MAKER_SECTIONS.PRODUCTS}>
       <SimpleGrid columns={3} spacing={10}>
-        {data.product.map(({ name, id: pid }) => (
+        {data.product.map(({ name, id: pid }: Product) => (
           <Link key={pid} href={`/maker/${id}/product/${pid}`} passHref>
             <Box cursor="pointer">
               <Box bg="red.100" height="180px"></Box>
-              <Text noOfLines="1" align="center">
+              <Text noOfLines={1} align="center">
                 {name}
               </Text>
             </Box>
