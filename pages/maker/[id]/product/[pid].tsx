2b561eb3ab95184214bb10464a154ffae67206d8
Jose Soto <jm.soto.sanchez@gmail.com>
2021-10-16 02:05:09 -0300

    remove empty space

--- pages/maker/[id]/product/[pid].tsx
+++ pages/maker/[id]/product/[pid].tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useQuery, useMutation } from '@apollo/client';
 import {
-  Box,
   Text,
   Modal,
   ModalOverlay,
