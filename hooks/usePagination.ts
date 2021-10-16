0803f5af8eb8416c3e33b97c38a747d8ca203a34
Jose Soto <jm.soto.sanchez@gmail.com>
2021-10-16 02:04:31 -0300

    pagination with size as param

--- hooks/usePagination.ts
+++ hooks/usePagination.ts
@@ -2,12 +2,16 @@ import { ApolloQueryResult } from '@apollo/client';
 import { useState, useEffect } from 'react';
 import { PAGINATION } from '@/utils/constants';
 
-export const usePagination = (data: any, refetch: (variables?: Partial<any>) => Promise<ApolloQueryResult<any>>) => {
+export const usePagination = (
+  data: any,
+  refetch: (variables?: Partial<any>) => Promise<ApolloQueryResult<any>>,
+  pageSize: number = PAGINATION.defaultSize
+) => {
   const [currentPage, setCurrentPage] = useState<number>(0);
 
   useEffect(() => {
-    if (data) refetch({ offset: PAGINATION.defaultSize * currentPage });
-  }, [data, currentPage, refetch]);
+    if (data) refetch({ offset: pageSize * currentPage });
+  }, [data, currentPage, refetch, pageSize]);
 
   return { currentPage, setCurrentPage };
 };
