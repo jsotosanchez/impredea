555d24d1ac1924b1268debc58a7d52fe339c7137
jmiguelsoto <jose.ssoto@mercadolibre.com>
2021-12-19 21:40:27 -0300

    catalog card update

--- pages/maker/[id]/catalog.tsx
+++ pages/maker/[id]/catalog.tsx
@@ -3,7 +3,14 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useQuery } from '@apollo/client';
-import { Box, Text, SimpleGrid } from '@chakra-ui/react';
+import {
+  Box, Text, SimpleGrid,
+  Center,
+  useColorModeValue,
+  Heading,
+  Stack,
+  Image as ChakraImage,
+} from '@chakra-ui/react';
 import { GET_MAKER_CATALOG } from '@/graphql/queries';
 import { Layout } from '@/components/makerPage';
 import { Authorization, EmptyResults, LoadingPage, PaginationButtons } from '@/components/common';
@@ -38,12 +45,13 @@ export default function Catalog(): JSX.Element {
             <SimpleGrid columns={3} spacing={7}>
               {data.product.map(({ name, id: pid }: Product) => (
                 <Link key={pid} href={`/maker/${id}/product/${pid}`} passHref>
-                  <Box cursor="pointer">
+                  {/* <Box cursor="pointer">
                     <Image priority={true} src={`${BUCKET_FILES_URL}products/${pid}`} width="370px" height="240px" alt="" />
                     <Text noOfLines={1} align="center">
                       {name}
                     </Text>
-                  </Box>
+                  </Box> */}
+                  <ProductSimple title={name} pid={pid} />
                 </Link>
               ))}
             </SimpleGrid>
@@ -54,5 +62,66 @@ export default function Catalog(): JSX.Element {
         </>
       </Layout>
     </Authorization>
+  );
+}
+
+interface CardProps {
+  title: string;
+  pid: number
+}
+
+const IMAGE =
+  'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80';
+
+function ProductSimple({ title, pid }: CardProps) {
+  return (
+    <Center py={12}>
+      <Box
+        role={'group'}
+        p={6}
+        maxW={'330px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'gray.800')}
+        boxShadow={'2xl'}
+        rounded={'lg'}
+        pos={'relative'}
+        zIndex={1}>
+        <Box
+          rounded={'lg'}
+          mt={-12}
+          pos={'relative'}
+          height={'230px'}
+          _after={{
+            transition: 'all .3s ease',
+            content: '""',
+            w: 'full',
+            h: 'full',
+            pos: 'absolute',
+            top: 5,
+            left: 0,
+            backgroundImage: `${BUCKET_FILES_URL}products/${pid}`,
+            filter: 'blur(15px)',
+            zIndex: -1,
+          }}
+          _groupHover={{
+            _after: {
+              filter: 'blur(20px)',
+            },
+          }}>
+          <ChakraImage
+            rounded={'lg'}
+            height={230}
+            width={282}
+            objectFit={'cover'}
+            src={`${BUCKET_FILES_URL}products/${pid}`}
+          />
+        </Box>
+        <Stack pt={10} align={'center'}>
+          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
+            {title}
+          </Heading>
+        </Stack>
+      </Box>
+    </Center>
   );
 }
